64d449a7398ea5541586640722ec2de716848a73
RPSIOErick <ecerickbastos@gmail.com>
2024-08-08 12:46:29 -0300

    Single Page Application

---
+++ js/navbar.js
@@ -0,0 +1,12 @@
+export class NavBar{
+    constructor() {
+        this.elemento = document.querySelector('nav')
+    }
+
+    carregar() {
+        this.elemento.innerHTML = `
+            <a href='#home' id='home-link'> Home </a>
+            <a href='#contato' id='contato-link'> Contato </a>
+        `
+    }
+}
